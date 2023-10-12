5bb97c8c01fc5094f90e2f1bb34b1be0e6cd1c92
Matviienko Ruslana <brauniesrus@gmail.com>
2023-10-12 12:24:29 +0300

    fgdfg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy } from 'react';
+import { Layout } from './Layout';
 
 const Home = lazy(() => import('../pages/Home'));
 const MoviesPage = lazy(() => import('../pages/MoviesPage'));
 const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 const CastList = lazy(() => import('./CastList'));
 const Reviews = lazy(() => import('./Reviews'));
-const Layout = lazy(() => import('./Layout'));
 
 export const App = () => {
   return (
